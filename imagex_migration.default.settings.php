93318f4482fcfd9a4504098101b641a355618df2
amcgowanca <me@amcgowan.ca>
2013-09-09 13:47:11 -0700

    Core: Initial module commit.

---
+++ imagex_migration.default.settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ */
+
+// TODO: Add documentation regarding this.
+$settings['source']['sql']['connections'] = array(
+  'source_default' => array(
+    'default' => array (
+      'database' => '',
+      'username' => '',
+      'password' => '',
+      'host' => '',
+      'port' => '',
+      'driver' => '',
+      'prefix' => '',
+    ),
+  )
+);
